8e7f7714a8ded1d33cfae551ca95173a52864709
Zhenzhen Xiang <zhenzhen.xiang@lingzhitech.com>
2018-07-05 10:07:30 +0800

    replace node args with parameters

--- src/camera_pipe_node.cpp
+++ src/camera_pipe_node.cpp
@@ -16,15 +16,10 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "camera_pipe_node");
   ros::NodeHandle nh;
 
-  if(argc < 5){
-    ROS_ERROR("Four args are needed: path_to_IMGPipe, number of cameras, image_width, image_height");
-    return 1;
-  }
-
-  std::string path_IMGPipe = argv[1]; //Default: "/tmp/_ap0102_pixel"
-  int cam_num = atoi(argv[2]); //Default: 4
-  int image_width = atoi(argv[3]); //Default: 1280
-  int image_height = atoi(argv[4]); //Default: 960
+  std::string path_IMGPipe = nh.param("camera_pipe/pipe_path", std::string("/tmp/_ap0102_pixel"));
+  int cam_num = nh.param("cam_num", 4);
+  int image_width = nh.param("image_width", 1280);
+  int image_height = nh.param("image_height", 960);
 
   CameraPipe cp(nh, path_IMGPipe, cam_num, image_width, image_height);
 
