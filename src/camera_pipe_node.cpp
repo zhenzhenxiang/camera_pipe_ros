ce12026165384520526361133e8202197d71423d
Zhenzhen Xiang <zhenzhen.xiang@lingzhitech.com>
2018-07-05 10:26:36 +0800

    add nodehandler for reading params

--- src/camera_pipe_node.cpp
+++ src/camera_pipe_node.cpp
@@ -15,11 +15,12 @@ int main(int argc, char** argv){
 
   ros::init(argc, argv, "camera_pipe_node");
   ros::NodeHandle nh;
+  ros::NodeHandle pnh("~");
 
   std::string path_IMGPipe = nh.param("camera_pipe_path", std::string("/tmp/_ap0102_pixel"));
-  int cam_num = nh.param("cam_num", 4);
-  int image_width = nh.param("image_width", 1280);
-  int image_height = nh.param("image_height", 960);
+  int cam_num = pnh.param("cam_num", 4);
+  int image_width = pnh.param("image_width", 1280);
+  int image_height = pnh.param("image_height", 960);
 
   CameraPipe cp(nh, path_IMGPipe, cam_num, image_width, image_height);
 
